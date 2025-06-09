b717283ce8c8c67fcb2bb7103110bceb602efac0
rahul-charaki <charakirahul@gmail.com>
2025-06-09 16:30:46 +0530

    Mobile verifier is added

---
+++ src/lyik/mobile_verifier/mobile_verification.py
@@ -0,0 +1,36 @@
+from apluggy import pluggy
+from lyikpluginmanager import getProjectName, VerifyHandlerSpec, VerifyHandlerResponseModel, VERIFY_RESPONSE_STATUS, ContextModel
+from lyikpluginmanager.annotation import RequiredEnv
+import logging
+from ..model.sampleform import RootMobileVerifierMobile
+import re
+
+logger = logging.getLogger(__name__)
+
+impl = pluggy.HookimplMarker(getProjectName())
+
+def is_valid_mobile_number(mobile_number: str) -> bool:
+    """
+    Validate the mobile number format.
+    This example checks for a 10-digit number, but you can adjust the regex as needed.
+    """
+    pattern = re.compile(r"^[6-9]\d{9}$")
+    return bool(pattern.match(mobile_number))
+
+class MobileVerification(VerifyHandlerSpec):
+    @impl
+    async def verify_handler(self, context:ContextModel, payload:RootMobileVerifierMobile) -> VerifyHandlerResponseModel:
+
+        if is_valid_mobile_number(payload.contact_id) :
+            
+            return VerifyHandlerResponseModel(
+                status=VERIFY_RESPONSE_STATUS.SUCCESS,
+                message="Mobile verification successful",
+                
+            )
+        else:
+            return VerifyHandlerResponseModel(
+                status=VERIFY_RESPONSE_STATUS.FAILURE,
+                message="Invalid mobile number format",
+            )
+        
