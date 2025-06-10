6ed1abb39e6e5dccb5c3ea93c4a6989908ab8be0
rahul-charaki <charakirahul@gmail.com>
2025-06-10 12:48:18 +0530

    added sample user verifier

--- src/lyik/mobile_verifier/mobile_verification.py
+++ src/lyik/mobile_verifier/mobile_verification.py
@@ -2,7 +2,7 @@ import apluggy as pluggy
 from lyikpluginmanager import getProjectName, VerifyHandlerSpec, VerifyHandlerResponseModel, VERIFY_RESPONSE_STATUS, ContextModel
 from lyikpluginmanager.annotation import RequiredEnv
 import logging
-from ..model.sampleform import RootMobileVerifier
+from ..model.sampleform import RootMobileVerifierInfo, RootUserDetailsInfo
 import re
 
 logger = logging.getLogger(__name__)
@@ -20,7 +20,7 @@ def is_valid_mobile_number(mobile_number: str) -> bool:
 
 class MobileVerification(VerifyHandlerSpec):
     @impl
-    async def verify_handler(self, context:ContextModel, payload:RootMobileVerifier) -> VerifyHandlerResponseModel:
+    async def verify_handler(self, context:ContextModel, payload:RootMobileVerifierInfo) -> VerifyHandlerResponseModel:
 
         if is_valid_mobile_number(payload.mobile.contact_id):
             
@@ -33,5 +33,21 @@ class MobileVerification(VerifyHandlerSpec):
             return VerifyHandlerResponseModel(
                 status=VERIFY_RESPONSE_STATUS.FAILURE,
                 message="Invalid mobile number format",
+            )
+        
+
+class UserVerification(VerifyHandlerSpec):
+    @impl
+    async def verify_handler(self, context:ContextModel, payload:RootUserDetailsInfo) -> VerifyHandlerResponseModel:
+        if payload.name and payload.email is not None:
+            return VerifyHandlerResponseModel(
+                status=VERIFY_RESPONSE_STATUS.SUCCESS,
+                message="User verification successful",
+                data={"name": payload.name},
+            )
+        else:
+            return VerifyHandlerResponseModel(
+                status=VERIFY_RESPONSE_STATUS.FAILURE,
+                message="User verification failed: Name or Email is missing",
             )
         
