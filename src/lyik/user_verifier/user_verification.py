9954fa27bbc753c0ba0f96afff2ded7164bafcb2
rahul-charaki <charakirahul@gmail.com>
2025-06-11 16:03:08 +0530

    added personal verifier

---
+++ src/lyik/user_verifier/user_verification.py
@@ -0,0 +1,28 @@
+import apluggy as pluggy
+from lyikpluginmanager import getProjectName, VerifyHandlerSpec, VerifyHandlerResponseModel, VERIFY_RESPONSE_STATUS, ContextModel
+from lyikpluginmanager.annotation import RequiredEnv
+import logging
+from ..model.jobapplicationform import RootPersonalInfo
+
+logger = logging.getLogger(__name__)
+
+impl = pluggy.HookimplMarker(getProjectName())
+
+class UserVerification(VerifyHandlerSpec):
+    @impl
+    async def verify_handler(self, context:ContextModel, payload:RootPersonalInfo) -> VerifyHandlerResponseModel:
+        """
+        Verify user details such as name and email.
+        """
+        if payload.name and payload.email:
+            return VerifyHandlerResponseModel(
+                status=VERIFY_RESPONSE_STATUS.SUCCESS,
+                message="User verification successful",
+                data={"name": payload.name},
+            )
+        else:
+            return VerifyHandlerResponseModel(
+                status=VERIFY_RESPONSE_STATUS.FAILURE,
+                message="User verification failed: Name or Email is missing",
+            )
+        
