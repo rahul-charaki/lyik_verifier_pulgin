77e9af6347e548eb85773b63a5c9740868786d17
rahul-charaki <charakirahul@gmail.com>
2025-06-11 16:13:14 +0530

    changed payload

--- src/lyik/user_verifier/user_verification.py
+++ src/lyik/user_verifier/user_verification.py
@@ -2,7 +2,7 @@ import apluggy as pluggy
 from lyikpluginmanager import getProjectName, VerifyHandlerSpec, VerifyHandlerResponseModel, VERIFY_RESPONSE_STATUS, ContextModel
 from lyikpluginmanager.annotation import RequiredEnv
 import logging
-from ..model.jobapplicationform import RootPersonalInfo
+from ..model.jobapplicationform import RootPersonalInfoPerInfoCard
 
 logger = logging.getLogger(__name__)
 
@@ -10,7 +10,7 @@ impl = pluggy.HookimplMarker(getProjectName())
 
 class UserVerification(VerifyHandlerSpec):
     @impl
-    async def verify_handler(self, context:ContextModel, payload:RootPersonalInfo) -> VerifyHandlerResponseModel:
+    async def verify_handler(self, context:ContextModel, payload:RootPersonalInfoPerInfoCard) -> VerifyHandlerResponseModel:
         """
         Verify user details such as name and email.
         """
@@ -18,7 +18,7 @@ class UserVerification(VerifyHandlerSpec):
             return VerifyHandlerResponseModel(
                 status=VERIFY_RESPONSE_STATUS.SUCCESS,
                 message="User verification successful",
-                data={"name": payload.name},
+                data={"name": payload.first_name, "email": payload.email},
             )
         else:
             return VerifyHandlerResponseModel(
